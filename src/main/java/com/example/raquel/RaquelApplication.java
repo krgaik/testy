257657b48207c044faeea23006ea078ef96c110f
krgai <k.r.gaik@gmail.com>
2020-03-29 19:04:57 +0200

    testy

---
+++ src/main/java/com/example/raquel/RaquelApplication.java
@@ -0,0 +1,13 @@
+package com.example.raquel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaquelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaquelApplication.class, args);
+    }
+
+}
